96b6ff6bb7780db09fba76b67c2b87364b022229
Wu Yue <351830907@qq.com>
2020-07-18 21:31:59 +0800

    修改了首页

---
+++ src/components/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="layout">
+    <Layout>
+      <Header>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+            <MenuItem name="1">
+              <Icon type="ios-navigate"></Icon>Item 1
+            </MenuItem>
+            <MenuItem name="2">
+              <Icon type="ios-keypad"></Icon>Item 2
+            </MenuItem>
+            <MenuItem name="3">
+              <Icon type="ios-analytics"></Icon>Item 3
+            </MenuItem>
+            <MenuItem name="4">
+              <Icon type="ios-paper"></Icon>Item 4
+            </MenuItem>
+        </Menu>
+      </Header>
+      <Content :style="{padding: '0 50px'}">
+        <Breadcrumb :style="{margin: '20px 0'}">
+          <BreadcrumbItem>Home</BreadcrumbItem>
+          <BreadcrumbItem>Components</BreadcrumbItem>
+          <BreadcrumbItem>Layout</BreadcrumbItem>
+        </Breadcrumb>
+        <Card>
+          <div style="min-height: 200px;">Content</div>
+        </Card>
+      </Content>
+      <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
+    </Layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+  display:flex;
+  justify-content: center;
+}
+.layout-logo {
+  width: 100px;
+  height: 30px;
+  background: #5b6270;
+  border-radius: 3px;
+  float: left;
+  position: relative;
+  top: 15px;
+  left: 20px;
+}
+.layout-footer-center {
+  text-align: center;
+}
+
+.ivu-menu{
+    width: 420px;
+    margin:0 auto;
+    background: rgba(40,42,44,0);
+}
+
+.ivu-layout-header{
+    background: rgba(40,42,44,.4);
+}
+.ivu-menu-item{
+    color:rgba(255, 255, 255, 1) !important
+}
+
+</style>
